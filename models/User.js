5bba34632a632fa42cfa41f5096eaf887dd65f92
ngencin <niademe@gmail.com>
2023-01-20 20:40:27 +0100

    gitignore images

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 // Modèle qui empêche un utilisateur d'utiliser la même adresse mail
 const mongoose = require('mongoose'); // 
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator') // assure que l'email enregistré dans la base de données est unique
 
 const userSchema = mongoose.Schema({ // fonction schema, informations a stocker
   email: { type: String, required: true, unique: true }, // unique = pas de possibilités d'inscription avec le même email
